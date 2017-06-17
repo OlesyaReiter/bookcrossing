b823990670ba9096e7045907468714e8f3b33d6a
Olesya Raiter <olesya.reiter@gmail.com>
2017-06-17 22:55:47 +0300

    Login step 3

--- js/modal.js
+++ js/modal.js
@@ -17,7 +17,34 @@ function showIntroduceForm(){
 function Login(){
     var name = document.getElementById("nickName").value;
     localStorage.setItem("nickName", name);
-
-    alert(localStorage.getItem("nickName"));
+    var myProfileTitle = document.getElementById("myProfileTitle");
+    myProfileTitle.innerHTML += ' (' + localStorage.getItem("nickName") + ')';
+    document.getElementById("nickName").value = '';
     closeModal();
+
+    showMyRoomButton(true);
+}
+
+//If we are loged in we show 'Create new entry' and 'Logout' buttons, otherwise show only 'Introduce' button
+function showMyRoomButton(isLogin){
+    var newEntryButton = document.getElementById('newEntryButton');
+    var introduceButton = document.getElementById('introduceButton');
+    var logoutButton = document.getElementById('logoutButton');
+    if (isLogin){
+        newEntryButton.classList.remove('hide-button');
+        logoutButton.classList.remove('hide-button');
+        introduceButton.classList.add('hide-button');
+    } else{
+        newEntryButton.classList.add('hide-button');
+        logoutButton.classList.add('hide-button');
+        introduceButton.classList.remove('hide-button');
+    }
+}
+
+function logout(){
+    localStorage.removeItem("nickName");
+
+    var myProfileTitle = document.getElementById("myProfileTitle");
+    myProfileTitle.innerHTML = 'My Profile';
+    showMyRoomButton(false);
 }
